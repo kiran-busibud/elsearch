b67e61825653602013ef6e36caa7fb26fcefaa80
kiran-busibud <kiran@zinosi.com>
2023-03-17 15:05:05 +0530

    queue implementation

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Ticket;
+use Illuminate\Support\Carbon;
+use App\Jobs\ElasticsearchIndexJob;
 
 class IndexController extends Controller
 {
-    //
+    public function index(Request $request){
+        $params = request()->all();
+        $ticket = Ticket::create([
+            'user' => 'John Doe',
+            'email' => 'john@example.com',
+            'date' => '2023-03-03'
+        ]);
+        $ticketEs = new Ticket();
+        $ticketEs->user = 'John Doe1';
+        $ticketEs->email = 'john@example.com1';
+        $ticketEs->date = Carbon::now();
+        $ticketEs->searchable();
+        ElasticsearchIndexJob::dispatch($$ticketEs)->onQueue('elasticsearch');
+    }
 }
